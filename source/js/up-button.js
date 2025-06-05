f0e03b21e097173fc6e7ef798dd5e83e3d0d22ef
vbratsun <vbratsun@gmail.com>
2025-06-05 21:28:23 +0300

    внесение изменений в карточки товаров, верстка страницы Сведения об организации

--- source/js/up-button.js
+++ source/js/up-button.js
@@ -1,25 +1,24 @@
-const upButton = document.querySelector('.page-body__up-button');
-const targetSection = document.getElementById('services'); // Секция, при достижении которой появится кнопка
+const upButton = document.querySelector(".page-body__up-button");
 
 const onUpButtonClick = () => {
   window.scrollTo({
     top: 0,
-    left: 0,
     behavior: "smooth",
   });
 };
 
 const onWindowScroll = () => {
-  if (!targetSection) return; // Проверка на существование элемента
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+  const halfPage = document.documentElement.scrollHeight / 2;
 
-  const sectionPosition = targetSection.getBoundingClientRect().top; // Позиция секции относительно вьюпорта
-
-  if (sectionPosition <= window.innerHeight / 2) { // Когда секция попадает в середину экрана
-    upButton.classList.add('page-body__up-button--show');
+  if (scrollTop > 110) {
+    upButton.classList.add("page-body__up-button--show");
   } else {
-    upButton.classList.remove('page-body__up-button--show');
+    upButton.classList.remove("page-body__up-button--show");
   }
 };
 
-upButton.addEventListener('click', onUpButtonClick);
-window.addEventListener('scroll', onWindowScroll);
+if (upButton) {
+  upButton.addEventListener("click", onUpButtonClick);
+  window.addEventListener("scroll", onWindowScroll);
+}
